0f096a87da53fa3a7eeb02b7d1e1206b6cea567f
Hieu <mr.hieund@home.com>
2015-01-11 17:29:12 +0800

    fix autoload window dir format

--- src/public/autoload.php
+++ src/public/autoload.php
@@ -4,5 +4,6 @@ spl_autoload_extensions(".php"); // comma-separated list
 spl_autoload_register('autoload');
 
 function autoload($class) {
-    require_once '..\\'.  ucfirst($class) . '.php';
+    $class = str_replace('\\', '/', $class);
+    require_once '../'.  ucfirst($class) . '.php';
 }
